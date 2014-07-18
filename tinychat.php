241eb9df75a55a92d493e3e48702a7e0790ee174
ruddernation <ruddernation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-18 06:46:24 +0000

    Core updates, Tested and is working on 4.0.
    git-svn-id: https://plugins.svn.wordpress.org/wpbp-video-chat/trunk@950818 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinychat.php
+++ tinychat.php
@@ -8,30 +8,23 @@
 This also has YouTube/SoundCloud for all chatters and now has smileys enabled using my embed file.
 * Requires at least: WordPress 3.6.0, BuddyPress 1.8.1
 * Tested up to: WordPress 4.0-beta1 / BuddyPress 2.0.1
-* Version: 1.0.9
+* Version: 1.1.0
 * License: GPL
 * License URI: http://www.gnu.org/licenses/gpl.html
 * Date: 10th July 2014
 */
-
 define('COMPARE_VERSION', '1.0.9');
 
-register_activation_hook(__FILE__, 'tinychat_install');
+register_activation_hook(__FILE__, 'wordpress_chat_install');
 
-function tinychat_install() {
+function wordpress_chat_install() {
 
 	global $wpdb, $wp_version;
-
 	$post_date = date("Y-m-d H:i:s");
-
 	$post_date_gmt = gmdate("Y-m-d H:i:s");
-
 	$sql = "SELECT * FROM ".$wpdb->posts." WHERE post_content LIKE '%[tinychat_page]%' AND `post_type` NOT IN('revision') LIMIT 1";
-
 	$page = $wpdb->get_row($sql, ARRAY_A);
-
 	if($page == NULL) {
-
 		$sql ="INSERT INTO ".$wpdb->posts."(
 
 			post_author, post_date, post_date_gmt, post_content, post_content_filtered, post_title, post_excerpt,  post_status, comment_status, ping_status, post_password, post_name, to_ping, pinged, post_modified, post_modified_gmt, post_parent, menu_order, post_type)
@@ -41,38 +34,28 @@ function tinychat_install() {
 			('1', '$post_date', '$post_date_gmt', '[tinychat_page]', '', 'chatroom', '', 'publish', 'closed', 'closed', '', 'chatroom', '', '', '$post_date', '$post_date_gmt', '0', '0', 'page')";
 
 		$wpdb->query($sql);
-
 		$post_id = $wpdb->insert_id;
-
 		$wpdb->query("UPDATE $wpdb->posts SET guid = '" . get_permalink($post_id) . "' WHERE ID = '$post_id'");
-
 	} else {
-
 		$post_id = $page['ID'];
-
 	}
-
-	update_option('tinychat_chat_url', get_permalink($post_id));
+	update_option('wordpress_chat_url', get_permalink($post_id));
 }
+add_filter('the_content', 'wp_show_wordpress_chat_page', 12);
 
-
-add_filter('the_content', 'wp_show_tinychat_page', 12);
-
-function wp_show_tinychat_page($content = '') {
+function wp_show_wordpress_chat_page($content = '') {
 
 	if(preg_match("/\[tinychat_page\]/",$content)) {
 
-		wp_show_tinychat();
+		wp_show_wordpress_chat();
 
 		return "";
 	}
-
 	return $content;
 }
+function wp_show_wordpress_chat() {
 
-function wp_show_tinychat() {
-
-	if(!get_option('tinychat_chat_enabled', 0)) {
+	if(!get_option('wordpress_chat_enabled', 0)) {
 	}
 	?>
 	<style>#chat{height:98%;width:100%;left:0px; right:0px; bottom:0px; position:absolute;}</style>
@@ -87,5 +70,4 @@ embed = tinychat ({room: "<?php echo htmlspecialchars($_SERVER['SERVER_NAME']) ?
 	</script><div id='client'></div></div>
 	<?php
 }
-
 ?>
